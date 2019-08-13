27ae81b93d96054f1ee722792b9fc9fbb84db678
Anastasia Skripnikova <skripnikovanastia@gmail.com>
2019-08-13 16:16:09 +0200

    Ispravila

--- db/migrate/20190813134407_create_meal_ingredients.rb
+++ db/migrate/20190813134407_create_meal_ingredients.rb
@@ -1,9 +1,9 @@
 class CreateMealIngredients < ActiveRecord::Migration[5.2]
   def change
     create_table :meal_ingredients do |t|
-      t.integer :meal_id,  null: false
-      t.integer :ingredient_id,  null: false
-      t.integer :quantity,  null: false
+      t.integer :meal_id, null: false
+      t.integer :ingredient_id, null: false
+      t.integer :quantity, null: false
 
       t.timestamps
     end
