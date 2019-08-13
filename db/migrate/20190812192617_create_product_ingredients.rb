880a4e55cd8c171cb16af459c55770cc972a6fca
Anastasia Skripnikova <skripnikovanastia@gmail.com>
2019-08-13 12:48:31 +0200

    Add Product model, ProductIngredient model

---
+++ db/migrate/20190812192617_create_product_ingredients.rb
@@ -0,0 +1,15 @@
+class CreateProductIngredients < ActiveRecord::Migration[5.2]
+  def change
+    create_table :product_ingredients do |t|
+      t.integer :product_id, null: false
+      t.integer :ingredient_id, null: false
+      t.timestamps
+    end
+
+    add_index :product_ingredients, :product_id
+    add_index :product_ingredients, :ingredient_id
+    
+    add_foreign_key :product_ingredients, :products 
+    add_foreign_key :product_ingredients, :ingredients
+  end
+end
